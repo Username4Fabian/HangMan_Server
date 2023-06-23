0f9fd15e5f01906dd35080455a0758ec63c37073
Username4Fabian <03anion.proxys@icloud.com>
2023-06-23 11:59:01 +0200

    added FileRead

---
+++ GameController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "./resources/words.txt";
+        List<string> words = ReadFile(filePath);
+
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+        Console.WriteLine(words.Count);
+    }
+
+  public static List<string> ReadFile(string path)
+    {
+        List<string> words = new List<string>();
+
+        try
+        {
+            // Open the file for reading
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string? line;
+                // Read each line until the end of the file
+                while ((line = sr.ReadLine()) != null)
+                {
+                    words.Add(line);
+                }
+                sr.Close(); 
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occurred while reading the file: " + e.Message);
+        }
+
+        return words;
+    }
+}
