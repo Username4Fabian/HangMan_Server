92a31b7b49c1868ea3b0f07f34435b787843d90d
Username4Fabian <108352227+Username4Fabian@users.noreply.github.com>
2023-06-24 13:58:35 +0200

    boyyyysss... We did the messing up

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Hangman_Server.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ RandomWordGenerator_temp.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using  Hangman_Server.Models;
+
+class RandomWordGenerator_temp
+{
+    public static String Manager(){
+        string path = Path.Combine(Directory.GetCurrentDirectory(), "resources", "words.txt");
+        List<string> words = ReadFile(path);
+        string word = GetRandomWord(words);
+        return word;
+    }
+  public static List<string> ReadFile(String path)
+    {
+        List<string> words = new List<string>();
+
+        try
+        {
+            // Open the file for reading
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string? line;
+                // Read each line until the end of the file
+                while ((line = sr.ReadLine()) != null)
+                {
+                    words.Add(line);
+                }
+                sr.Close(); 
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occurred while reading the file: " + e.Message);
+        }
+
+        return words;
+    }
+
+public static string GetRandomWord(List<string> words)
+{
+    if (words.Count == 0)
+    {
+        // Handle the case when the list is empty
+        Console.WriteLine("The list of words is empty.");
+        return "null"; // or return a default value
+    }
+    Random random = new Random();
+    int index = random.Next(words.Count);
+    return words[index];
+}
+}
+
